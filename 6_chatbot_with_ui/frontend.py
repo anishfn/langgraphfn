ff149788a271298a86adcc97fe62617d280fdd95
Anish Gupta <anishhfn@gmail.com>
2026-04-18 12:59:17 +0530

    added streaming chat feature

--- 6_chatbot_with_ui/frontend.py
+++ 6_chatbot_with_ui/frontend.py
@@ -27,8 +27,16 @@ if user_input:
 
 
 
-  response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=config)
-  ai_message = response['messages'][-1].content
-  st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+  # response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=config)
+  # ai_message = response['messages'][-1].content
   with st.chat_message("assistant"):
-    st.text(ai_message)
+    # st.text(ai_message)
+    ai_message = st.write_stream(
+      message_chunk.content for message_chunk, metadata in chatbot.stream(
+        {'messages': [HumanMessage(content=user_input)]},
+        config=config,
+        stream_mode="messages"
+      )
+    )
+
+  st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
