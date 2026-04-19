15863175f2e449c412853a790a4d5492ddd687db
Anish Gupta <anishhfn@gmail.com>
2026-04-19 15:14:17 +0530

    day05: added sqlite checkpointer for chatbot

--- 6_chatbot_with_ui/backend.py
+++ 6_chatbot_with_ui/backend.py
@@ -2,9 +2,10 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
 from langchain_mistralai import ChatMistralAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -15,6 +16,9 @@ llm = ChatMistralAI()
 class ChatState(TypedDict):
   messages: Annotated[list[BaseMessage], add_messages]
 
+# sqlite3 db
+conn = sqlite3.connect(database="chatbot.db", check_same_thread=False)
+
 # node functions
 def chat_node(state: ChatState) -> ChatState:
   messages = state['messages']
@@ -22,7 +26,7 @@ def chat_node(state: ChatState) -> ChatState:
   return {'messages': [response]}
 
 # checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=conn)
 
 # graph
 graph = StateGraph(ChatState)
@@ -36,3 +40,12 @@ graph.add_edge("chat_node", END)
 
 # compile graph
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# retrieve all threads function
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
